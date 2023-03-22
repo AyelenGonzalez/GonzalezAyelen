9c24b57883eed4fee096af03164845380e917e21
Usuario <usuario@iMac-de-Aye-Gonzalez.local>
2023-03-22 10:39:11 -0300

    SegundaEntrega

--- index.js
+++ index.js
@@ -1,49 +1,61 @@
+const fs = require('fs');
+
 class ProductManager {
-  constructor() {
-    this.products = [];
-    this.currentId = 0;
+  constructor(filePath) {
+    this.filePath = filePath;
   }
 
-  addProduct(product) {
-    // Validamos que todos los campos sean obligatorios
-    if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
-      console.log('Error: todos los campos son obligatorios');
-      return;
-    }
-
-    // Validamos que el campo "code" no esté repetido
-    const codeExists = this.products.some(p => p.code === product.code);
-    if (codeExists) {
-      console.log('Error: ya existe un producto con ese código');
-      return;
-    }
-
-    // Creamos el producto con un id autoincrementable
+  addProduct = (product) => {
+    const products = this.getProducts();
     const newProduct = {
-      id: ++this.currentId,
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      thumbnail: product.thumbnail,
-      code: product.code,
-      stock: product.stock
+      ...product,
+      id: products.length + 1,
     };
-
-    // Agregamos el producto al arreglo de productos
-    this.products.push(newProduct);
-    console.log('Producto agregado:', newProduct);
+    products.push(newProduct);
+    fs.writeFileSync(this.filePath, JSON.stringify(products));
+    return newProduct;
   }
 
-  getProducts() {
-    return this.products;
+  getProducts = () => {
+    try {
+      const products = fs.readFileSync(this.filePath, 'utf-8');
+      return JSON.parse(products);
+    } catch (error) {
+      console.error(`Error while reading file: ${error}`);
+      return [];
+    }
   }
 
-  getProductById(id) {
-    const product = this.products.find(p => p.id === id);
-    if (product) {
-      return product;
+  getProductById = (id) => {
+    const products = this.getProducts();
+    return products.find((product) => product.id === id);
+  }
+
+  updateProduct = (id, fieldsToUpdate) => {
+    const products = this.getProducts();
+    const index = products.findIndex((product) => product.id === id);
+    if (index !== -1) {
+      const updatedProduct = {
+        ...products[index],
+        ...fieldsToUpdate,
+        id,
+      };
+      products[index] = updatedProduct;
+      fs.writeFileSync(this.filePath, JSON.stringify(products));
+      return updatedProduct;
     } else {
-      console.log('Error: producto no encontrado');
+      throw new Error(`Product with id ${id} not found`);
+    }
+  }
+
+  deleteProduct = (id) => {
+    const products = this.getProducts();
+    const filteredProducts = products.filter((product) => product.id !== id);
+    if (filteredProducts.length !== products.length) {
+      fs.writeFileSync(this.filePath, JSON.stringify(filteredProducts));
+      return true;
+    } else {
+      throw new Error(`Product with id ${id} not found`);
     }
   }
 }
